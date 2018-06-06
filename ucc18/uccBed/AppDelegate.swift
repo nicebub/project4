e57da0b9d574a3a11e248f9ba9f6e7eaa7f21015
UNKLSC00TY <ugascottl@gmail.com>
2018-06-05 18:53:13 -0600

    gitignore added as well as the start of a GUI for loading files and seeing output.

---
+++ ucc18/uccBed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  uccBed
+//
+//  Created by Scott Lorberbaum on 6/1/18.
+//  Copyright © 2018 Laurel Tree Computing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ucc18/uccBed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  uccBed
+//
+//  Created by Scott Lorberbaum on 6/1/18.
+//  Copyright © 2018 Laurel Tree Computing. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
